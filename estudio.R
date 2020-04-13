a618359701cb0a3ac0725192667c404c2e430482
kamirvaldes <62215175+kamirvaldes@users.noreply.github.com>
2020-04-13 17:27:27 -0300

    clase 5 y estudio

--- estudio.R
+++ estudio.R
@@ -26,3 +26,38 @@ d<-casosRM[Sexo=='Masculino',.(total_hombres=.N), by= 'Centro de salud']
 ab<-merge(a,b,by = 'Centro de salud')
 abc<-merge(ab,c,by = 'Centro de salud')
 abcd<-merge(abc,d,by = 'Centro de salud')
+
+
+View(abcd)
+names(abcd)
+
+
+abcd[,.(pacientes= sum(total_mujeres,total_hombres)),by= 'Centro de salud']
+casosRM[Edad=='â€”', Edad== 0]
+
+casosRM[Sexo=='Femenino', .(promedioEdadMujer= mean(as.numeric(Edad),na.rm = T)), by='Centro de salud']
+casosRM[Sexo=='Masculino',.(maxEdadHombre= max(as.numeric(Edad),na.rm = T)), by= 'Centro de salud']
+
+
+
+library(readxl)
+
+casos <- data.table(read_excel('Ayud_02/COVID_19_up_to_29_02_2020_cases.xlsx'), stringsAsFactors = F)
+
+muertes <- data.table(read_excel('Ayud_02/COVID_19_up_to_29_02_2020_deaths.xlsx'), stringsAsFactors = F)
+
+mas_dias <- data.table(read_excel('Ayud_02/COVID_19_from_01_03_2020_to_29_03_2020_all.xlsx'), stringsAsFactors = F)
+
+
+casosymuertes<-merge(casos,muertes,by = c('day', 'month', 'year', 'countriesAndTerritories'), all = T, sort = F) 
+
+casosymuertes[countriesAndTerritories== "Brazil",.(countriesAndTerritories)]
+ 
+casosymuertes[,.(popData2018), by= 'countriesAndTerritories']  
+
+al_29_03_2020 <- rbind(casosymuertes,mas_dias)  
+  
+al_29_03_2020[, .(sumacasos= sum(cases)), by= 'countriesAndTerritories']  
+
+
+al_29_03_2020[cases,.(countriesAndTerritories)]  
